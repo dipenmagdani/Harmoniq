d144f1e47bc9a6001a85dc936dda2d96b6c27366
dipenmagdani <dipenmagdani@gmail.com>
2024-07-09 11:06:49 +0530

    Addedd API

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const { default: axios } = require('axios');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const { API_URL } = process.env;
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://harmoniq-theta.vercel.app", // replace with your Vercel app URL
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const handleApiRequest = async (req, res, endpoint) => {
@@ -17,44 +22,44 @@ const handleApiRequest = async (req, res, endpoint) => {
     });
     res.json(response.data);
   } catch (error) {
-    console.error('Error in handleApiRequest:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error in handleApiRequest:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-app.get('/api', (req, res) => {
-  res.json('Hello');
+app.get("/api", (req, res) => {
+  res.json("Hello");
 });
 
-app.get('/api/home', (req, res) =>
-  handleApiRequest(req, res, '/modules?lang=hindi')
+app.get("/api/home", (req, res) =>
+  handleApiRequest(req, res, "/modules?lang=hindi")
 );
-app.get('/api/album/:id', (req, res) =>
+app.get("/api/album/:id", (req, res) =>
   handleApiRequest(req, res, `/album?id=${req.params.id}`)
 );
-app.get('/api/playlist/:id', (req, res) =>
+app.get("/api/playlist/:id", (req, res) =>
   handleApiRequest(req, res, `/playlist?id=${req.params.id}`)
 );
-app.get('/api/artist/:id', (req, res) =>
+app.get("/api/artist/:id", (req, res) =>
   handleApiRequest(req, res, `/artist?id=${req.params.id}`)
 );
-app.get('/api/song/:id', (req, res) =>
+app.get("/api/song/:id", (req, res) =>
   handleApiRequest(req, res, `/song?id=${req.params.id}`)
 );
-app.get('/api/search', (req, res) => handleApiRequest(req, res, `/search/top`));
-app.get('/api/search/:name', (req, res) =>
+app.get("/api/search", (req, res) => handleApiRequest(req, res, `/search/top`));
+app.get("/api/search/:name", (req, res) =>
   handleApiRequest(req, res, `/search?q=${req.params.name}&n=100&page=1`)
 );
-app.get('/api/search/album/:q', (req, res) =>
+app.get("/api/search/album/:q", (req, res) =>
   handleApiRequest(req, res, `/search/albums?q=${req.params.q}&n=100&page=1`)
 );
-app.get('/api/search/playlist/:q', (req, res) =>
+app.get("/api/search/playlist/:q", (req, res) =>
   handleApiRequest(req, res, `/search/playlists?q=${req.params.q}&n=100&page=1`)
 );
-app.get('/api/search/song/:q', (req, res) =>
+app.get("/api/search/song/:q", (req, res) =>
   handleApiRequest(req, res, `/search/songs?q=${req.params.q}&n=100&page=1`)
 );
-app.get('/api/search/artist/:q', (req, res) =>
+app.get("/api/search/artist/:q", (req, res) =>
   handleApiRequest(req, res, `/search/artists?q=${req.params.q}&n=100&page=1`)
 );
 
