be80b62f91c045f9895e05bb0111e0ec409ae4da
dipenmagdani <dipenmagdani@gmail.com>
2024-07-18 19:12:18 +0530

    add: frontend url

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 5000;
 const { API_URL } = process.env;
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "*",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 const handleApiRequest = async (req, res, endpoint) => {
   try {
