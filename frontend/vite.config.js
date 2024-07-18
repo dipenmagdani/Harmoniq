62aba25c8ff2aa0abce2f325b59c479f374f5694
dipenmagdani <dipenmagdani@gmail.com>
2024-07-18 20:09:54 +0530

    changed vite json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,15 +8,6 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       port: 5173,
-      ...(mode === 'development' && {
-        proxy: {
-          '/api': {
-            target: env.VITE_API_URL,
-            changeOrigin: true,
-            secure: false,
-          },
-        },
-      }),
     },
   };
 });
