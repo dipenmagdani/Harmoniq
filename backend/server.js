b49ccc7514c44ae3bdf45d1a48abdf087b1ba3aa
dipenmagdani <dipenmagdani@gmail.com>
2024-08-18 14:12:51 +0530

    Added: Cookie Parser Middlewar

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const apiRouter = require("./routes/apiRoute");
 const userRouter = require("./routes/user");
 
 connectDB();
-app.use(cookie_parser());
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.use(cookie_parser());
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "OPTIONS"],
