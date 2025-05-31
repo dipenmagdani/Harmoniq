bb508827e35a001d149d5c33c6cde74a1a32300a
dipenmagdani <dipenmagdani@gmail.com>
2025-05-31 13:16:32 +0530

    fix path issue

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const verifyToken = () => {
+const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
 
   if (!token) {
     return res.status(403).json({ msg: "No token provided" });
   }
 
-  jwt.verify(token.split("  ")[1], process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({ msg: "Unauthorized" });
     }
