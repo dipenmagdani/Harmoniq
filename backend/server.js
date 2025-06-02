f6287d5804cf42b39d0e02a607cec6fab7508f38
dipenmagdani <dipenmagdani@gmail.com>
2025-06-02 16:07:15 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,18 @@ const userRouter = require("./routes/user");
 connectDB();
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(express.json());
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "*",
+  origin: [process.env.FRONTEND_URL ?? "*"],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.status(200).json({
     status: 200,
